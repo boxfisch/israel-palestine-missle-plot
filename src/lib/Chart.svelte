bdc585b0d76ce559ce57326f83c48080552c3198
boxfisch <ptr5044@gmail.com>
2023-10-14 21:35:33 -0400

    Adds basic chart setup

---
+++ src/lib/Chart.svelte
@@ -0,0 +1,25 @@
+<script>
+	import data from '../assets/data'
+
+	let width = 600
+	let height = 600
+</script>
+
+<div class="container" bind:clientHeight={height} bind:clientWidth={width}>
+	<svg>
+	</svg>
+</div>
+
+<style>
+	.container {
+		margin-inline: auto;
+		max-width: 700px;
+	}
+
+	svg {
+		overflow: visible;
+		height: 600px;
+		position: relative;
+		width: 100%;
+	}
+</style>
