e468ecd539fc352e052d3870d5445a3ac197981c
boxfisch <ptr5044@gmail.com>
2023-10-14 21:25:05 -0400

    Init

---
+++ src/App.svelte
@@ -0,0 +1,7 @@
+<script>
+  import Chart from "./lib/Chart.svelte";
+</script>
+
+<main>
+  <Chart />
+</main>
---
+++ src/assets/data.js
@@ -0,0 +1,104 @@
+export default [
+  { year: "2000-01-01", palestine_deaths: 70.75, israeli_deaths: 10.25 },
+  {
+    year: "2001-01-01",
+    palestine_deaths: 39.166666666666664,
+    israeli_deaths: 16.0,
+  },
+  {
+    year: "2002-01-01",
+    palestine_deaths: 86.0,
+    israeli_deaths: 34.916666666666664,
+  },
+  {
+    year: "2003-01-01",
+    palestine_deaths: 49.0,
+    israeli_deaths: 15.416666666666666,
+  },
+  {
+    year: "2004-01-01",
+    palestine_deaths: 65.5,
+    israeli_deaths: 8.666666666666666,
+  },
+  {
+    year: "2005-01-01",
+    palestine_deaths: 15.833333333333334,
+    israeli_deaths: 4.25,
+  },
+  {
+    year: "2006-01-01",
+    palestine_deaths: 55.416666666666664,
+    israeli_deaths: 1.6666666666666667,
+  },
+  {
+    year: "2007-01-01",
+    palestine_deaths: 32.083333333333336,
+    israeli_deaths: 1.0,
+  },
+  {
+    year: "2008-01-01",
+    palestine_deaths: 73.83333333333333,
+    israeli_deaths: 3.25,
+  },
+  {
+    year: "2009-01-01",
+    palestine_deaths: 86.33333333333333,
+    israeli_deaths: 0.75,
+  },
+  {
+    year: "2010-01-01",
+    palestine_deaths: 6.833333333333333,
+    israeli_deaths: 0.75,
+  },
+  {
+    year: "2011-01-01",
+    palestine_deaths: 9.75,
+    israeli_deaths: 0.8333333333333334,
+  },
+  {
+    year: "2012-01-01",
+    palestine_deaths: 21.25,
+    israeli_deaths: 0.5833333333333334,
+  },
+  {
+    year: "2013-01-01",
+    palestine_deaths: 3.1666666666666665,
+    israeli_deaths: 0.5,
+  },
+  {
+    year: "2014-01-01",
+    palestine_deaths: 189.91666666666666,
+    israeli_deaths: 7.083333333333333,
+  },
+  {
+    year: "2015-01-01",
+    palestine_deaths: 15.666666666666666,
+    israeli_deaths: 2.0,
+  },
+  {
+    year: "2016-01-01",
+    palestine_deaths: 9.583333333333334,
+    israeli_deaths: 1.0,
+  },
+  {
+    year: "2017-01-01",
+    palestine_deaths: 8.0,
+    israeli_deaths: 1.5833333333333333,
+  },
+  {
+    year: "2018-01-01",
+    palestine_deaths: 24.166666666666668,
+    israeli_deaths: 1.1666666666666667,
+  },
+  {
+    year: "2019-01-01",
+    palestine_deaths: 12.416666666666666,
+    israeli_deaths: 0.9166666666666666,
+  },
+  {
+    year: "2020-01-01",
+    palestine_deaths: 2.8333333333333335,
+    israeli_deaths: 0.25,
+  },
+  { year: "2021-01-01", palestine_deaths: 7.2, israeli_deaths: 0.6 },
+];
